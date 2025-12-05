9284afa2e618b1389b7b5e87800ad98dc27ca532
Shizol01 <wrzosek-krystian@wp.pl>
2025-12-05 23:16:30 +0100

    feat(company): add customer branch crud and list view with initialization

---
+++ apps/company/views/customer_branch.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+
+from ..forms import CustomerBranchForm
+from ..models import CustomerBranch, Customer
+
+
+class CustomerBranchListView(View):
+    def get(self, request):
+        branches = CustomerBranch.objects.all()
+        return render(request, 'company/customer_branch/customer_branch_list.html', {'branches': branches})
+
+
+class CustomerBranchAddView(View):
+
+    def get(self, request, customer_id):
+        form = CustomerBranchForm()
+        return render(request, 'company/customer_branch/customer_branch_add.html', {'form': form})
+
+    def post(self, request, customer_id):
+        customer = get_object_or_404(Customer, id=customer_id)
+        form = CustomerBranchForm(request.POST)
+
+        if form.is_valid():
+            branch = form.save(commit=False)
+            branch.customer = customer
+            branch.save()
+            return redirect('customer-detail', customer_id=customer.id)
+
+        return render(request, 'company/customer_branch/customer_branch_add.html', {'form': form})
+
+
+class CustomerBranchDetailView(View):
+    def get(self, request, branch_id):
+        customer_branch = get_object_or_404(
+            CustomerBranch, id=branch_id
+        )
+        return render(request, 'company/customer_branch/customer_branch_detail.html',
+                      {'customer_branch': customer_branch})
+
+
+class CustomerBranchUpdateView(View):
+    def get(self, request, branch_id):
+        branch = get_object_or_404(
+            CustomerBranch, id=branch_id
+        )
+        form = CustomerBranchForm(instance=branch)
+        return render(request, 'company/customer_branch/customer_branch_update.html', {'form': form})
+
+    def post(self, request, branch_id):
+        branch = get_object_or_404(
+            CustomerBranch, id=branch_id
+        )
+        form = CustomerBranchForm(request.POST, instance=branch)
+
+        if form.is_valid():
+            branch = form.save(commit=False)
+            branch.customer = branch.customer
+            branch.save()
+            return redirect('customer-detail', customer_id=branch.customer.id)
+
+        return render(request, 'company/customer_branch/customer_branch_update.html', {'form': form})
+
+
+class CustomerBranchDeleteView(View):
+    def get(self, request, branch_id):
+        branch = get_object_or_404(
+            CustomerBranch, id=branch_id
+        )
+        return render(request, 'company/customer_branch/customer_branch_delete.html', {'branch': branch})
+
+    def post(self, request, branch_id):
+        branch = get_object_or_404(
+            CustomerBranch, id=branch_id
+        )
+        customer_id = branch.customer.id
+        branch.delete()
+        return redirect('customer-detail', customer_id=customer_id)
