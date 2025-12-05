39d448324d9d94bcfde65da2095cc416d004d44b
Shizol01 <wrzosek-krystian@wp.pl>
2025-12-05 13:58:31 +0100

    feat(company): add carier crud and list views, add initialization of views

--- apps/company/views/carrier.py
+++ apps/company/views/carrier.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 
 from ..forms import CarrierForm
 from ..models import Carrier
+
+
+class CarrierListView(View):
+    def get(self, request):
+        carriers = Carrier.objects.all()
+        return render(request, 'company/carrier_list.html', {'carriers': carriers})
 
 
 class CarrierAddView(View):
@@ -17,17 +23,46 @@ class CarrierAddView(View):
         if form.is_valid():
             form.save()
             return redirect('dashboard')
-
         return render(request, 'company/carrier_add.html', {'form': form})
 
 
 class CarrierDetailView(View):
-    def get(self, request):
-        carriers = Carrier.objects.all()
-        return render(request, 'company/carrier_detail.html', {'carriers': carriers})
+    def get(self, request, carrier_id):
+        carrier = get_object_or_404(
+            Carrier, id=carrier_id
+        )
+        return render(request, 'company/carrier_detail.html', {'carrier': carrier})
 
 
 class CarrierUpdateView(View):
     def get(self, request, carrier_id):
-        carrier = Carrier.objects.get(id=carrier_id)
+        carrier = get_object_or_404(
+            Carrier, id=carrier_id
+        )
         form = CarrierForm(instance=carrier)
+        return render(request, 'company/carrier_update.html', {'form': form})
+
+    def post(self, request, carrier_id):
+        carrier = get_object_or_404(
+            Carrier, id=carrier_id
+        )
+        form = CarrierForm(request.POST, instance=carrier)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+        return render(request, 'company/carrier_update.html', {'form': form})
+
+
+class CarrierDeleteView(View):
+    def get(self, request, carrier_id):
+        carrier = get_object_or_404(
+            Carrier, id=carrier_id
+        )
+        return render(request, 'company/carrier_delete.html', {'carrier': carrier})
+
+    def post(self, request, carrier_id):
+        carrier = get_object_or_404(
+            Carrier, id=carrier_id
+        )
+        carrier.delete()
+        return redirect('dashboard')
