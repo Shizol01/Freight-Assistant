970f20c22ea0092e2c0997921d35f750160884fc
Shizol01 <wrzosek-krystian@wp.pl>
2025-12-05 13:59:13 +0100

    chore(company): add urls for views

--- apps/company/urls.py
+++ apps/company/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import CarrierAddView
+from .views import (CarrierAddView, CarrierDetailView,
+                      CarrierUpdateView, CarrierDeleteView,
+                      CarrierListView)
 
 urlpatterns = [
-    path('add/',CarrierAddView.as_view(), name='carrier-add'),
+    path('carrier/add/',CarrierAddView.as_view(), name='carrier-add'),
+    path('carrier/detail/<int:carrier_id>',CarrierDetailView.as_view(), name='carrier-detail'),
+    path('carrier/update/<int:carrier_id>',CarrierUpdateView.as_view(), name='carrier-update'),
+    path('carrier/delete/<int:carrier_id>',CarrierDeleteView.as_view(), name='carrier-delete'),
+    path('carrier/all/',CarrierListView.as_view(), name='carrier-list'),
 ]
