7c67c4760babbf9ac10b26e391ad7f8f85c5a3ca
Shizol01 <wrzosek-krystian@wp.pl>
2025-12-11 19:13:29 +0100

    tests(accounts): add test for user profile and password change

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,11 @@ import pytest
 from django.contrib.auth.models import User
 from django.test import Client
 
+
 @pytest.fixture
 def client():
     return Client()
+
 
 @pytest.fixture
 def user():
@@ -15,3 +17,21 @@ def user():
     user.set_password("testpassword")
     user.save()
     return user
+
+
+@pytest.fixture
+def profile(user):
+    profile = user.profile
+
+    profile.about_me = 'lorem ipsum dolor sit amet'
+    profile.job = 'test_job'
+    profile.country = 'test_country'
+    profile.address = 'test_address'
+    profile.phone = '111222333'
+    profile.twitter = 'test_twitter.com'
+    profile.facebook = 'test_facebook.com'
+    profile.instagram = 'test_instagram.com'
+    profile.linkedin = 'test_linkedin.com'
+    profile.save()
+
+    return profile
